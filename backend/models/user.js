fe72a02e8494ffa67ae61e4c2f3b0095afbb757e
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-26 22:15:54 +0400

    feat: added email and password to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const isEmail = require("validator/lib/isEmail");
+const isURL = require('validator/lib/isURL');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,11 +18,27 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     validate: {
-      validator: (v) => /https?:\/\/(w{3}\.)?[\w\-.~:/?#[\]@!$&'\\()*+,;=]/.test(v),
-      message: 'Не верно указан адрес.',
+      validator: (v) => isURL(v),
+      message: "Не верно указан адрес.",
     },
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "Неверно указана почта."
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => v.length>7,
+      message: "Минимальная длинна пароля 8 симолов."
+    },
+  },
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
