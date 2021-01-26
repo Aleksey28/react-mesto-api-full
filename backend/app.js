760955e4b66ce1ad31baae45ae32f5cdf1c3bf8c
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-27 01:06:56 +0400

    feat: added protected routes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cookieParser  = require('cookie-parser');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cards = require('./routes/cards');
-const users = require('./routes/users');
-const { createUser } = require("./controllers/users");
-const { login } = require("./controllers/users");
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cards = require("./routes/cards");
+const users = require("./routes/users");
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -22,18 +22,20 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '5fd3f4da730a4b61f8e0152c', // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: "5fd3f4da730a4b61f8e0152c", // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
   next();
 });
 
 app.use(requestLogger);
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post("/signin", login);
+app.post("/signup", createUser);
 
-app.use('/', cards);
-app.use('/', users);
+app.use(auth);
+
+app.use("/", cards);
+app.use("/", users);
 
 app.use(errorLogger);
 
@@ -44,8 +46,8 @@ app.use((err, req, res, next) => {
     .send({
       // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
-        ? 'Server was broken =('
-        : message,
+               ? "Server was broken =("
+               : message,
     });
   next();
 });
